bb50e70939662f7e5e7f78c48b9a1a01d2cc7e70
Paula Bueno <>
2021-01-02 16:33:20 -0300

    fundamentos

--- fundamentos/area_circunferencia.py
+++ fundamentos/area_circunferencia.py
@@ -1,7 +1,8 @@
-# Shebang - define o interpretador que será usado
 #!/usr/local/bin/python3
-pi = 3.14159
-raio = 15.3
-area = pi * (raio ** 2)
+# Shebang - define o interpretador que será usado
+import math
+
+raio = input('Informe o raio: ')
+area = math.pi * (float(raio) ** 2)
 
 print('Área do círculo: ', area)
